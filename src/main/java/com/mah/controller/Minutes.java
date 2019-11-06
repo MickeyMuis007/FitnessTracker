8430148e16a8c61f7bb4090d23a2e332cef7e15e
Michael <mihendricks1@gmail.com>
2019-11-06 17:29:20 +0200

    feat: View changing

---
+++ src/main/java/com/mah/controller/Minutes.java
@@ -0,0 +1,25 @@
+package com.mah.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mah.model.Exercise;
+
+@Controller
+public class Minutes {
+	
+	@RequestMapping(value="/addMinutes")
+	public String addMinutes(@ModelAttribute("exercise") Exercise exercise) {
+		System.out.println("exercise: " + exercise.getMinutes());
+		
+		return "redirect:addMoreMinutes.html";
+	}
+	
+	@RequestMapping(value="/addMoreMinutes")
+	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
+		System.out.println("exercising: " + exercise.getMinutes());
+		
+		return "addMinutes";
+	}
+}
